30e36fb977b5ed3cc7266a27a87b3766325f11d2
Thinh Nguyen <thinhntr@gmail.com>
2021-06-03 00:28:07 +0700

    Add tailwind directive to index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex items-center justify-center h-screen">
+      <div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        Centered Content
+      </div>
     </div>
   );
 }
-
-export default App;
