cfc603275a2a5ee1ae9e2f147c432980d0a9fac9
Thinh Nguyen <thinhntr@gmail.com>
2021-06-17 22:12:48 +0700

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import ImageModelProvider from "./ImageModelProvider";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <ImageModelProvider>
-    <App />
+      <App />
     </ImageModelProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
