8ef53842afd8c0bd70a5982aadd8d88c9aba72e9
Thinh Nguyen <thinhntr@gmail.com>
2021-07-04 12:53:17 +0700

    Move labels to a different hosting server

--- src/ImageModelProvider.jsx
+++ src/ImageModelProvider.jsx
@@ -11,20 +11,30 @@ export function useImageModel() {
 
 export default function ImageModelProvider({ children }) {
   const [imageModel, setImageModel] = useState(null);
+  const [labels, setLabels] = useState(null);
+
+  const url = "https://modelhosting.web.app/";
+  const version = "thinh_imagemodel_2";
 
   useEffect(() => {
-    loadGraphModel(
-      "https://modelhosting.web.app/thinh_imagemodel_3/model.json"
-    ).then((loadedModel) => {
+    loadGraphModel(url + version + "/model.json").then((loadedModel) => {
       setImageModel(loadedModel);
-      console.log("Model loaded successfully");
-      console.log("Loaded Model: ", loadedModel);
-      console.log("onImageChange", tf.memory());
+      // console.log("Model loaded successfully");
+      // console.log("Loaded Model: ", loadedModel);
+      // console.log("onImageChange", tf.memory());
     });
+
+    fetch(url + version + "/labels.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setLabels(data);
+        // console.log("Data downloaded successfully");
+        // console.log("Loaded data", data);
+      });
   }, []);
 
   return (
-    <ImageModelContext.Provider value={imageModel}>
+    <ImageModelContext.Provider value={[imageModel, labels]}>
       {children}
     </ImageModelContext.Provider>
   );
