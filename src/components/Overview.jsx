345dc3d61272e3f7ce1a446412b3f2b096ebee65
Thinh Nguyen <thinhntr@gmail.com>
2021-07-16 08:59:52 +0700

    Update style for Overview page

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,28 +2,28 @@ import MainContainer from "./MainContainer";
 function Overview() {
   return (
     <MainContainer>
-      <h1 className="text-2xl">Nhận diện hàng tạp hóa</h1>
-      <h2 className="font-bold">About us</h2>
-      <table>
-        <tbody>
-          <tr className="rounded-sm hover:bg-gray-700">
-            <td className="py-2 px-3">Nguyễn Trường Thịnh</td>
-            <td className="py-2 px-3">18521447</td>
-          </tr>
-          <tr className="rounded-sm hover:bg-gray-700">
-            <td className="py-2 px-3">Nguyễn Hoàng Thiên</td>
-            <td className="py-2 px-3">18521427</td>
-          </tr>
-          <tr className="rounded-sm hover:bg-gray-700">
-            <td className="py-2 px-3">Trần Hoàng Sơn</td>
-            <td className="py-2 px-3">18521351</td>
-          </tr>
-          <tr className="rounded-sm hover:bg-gray-700">
-            <td className="py-2 px-3">Đỗ Mạnh Quân</td>
-            <td className="py-2 px-3">18521283</td>
-          </tr>
-        </tbody>
-      </table>
+      <h1 className="text-3xl sm:text-5xl sm:leading-normal font-extrabold bg-clip-text text-transparent bg-gradient-to-tl from-green-400 to-blue-500">
+        Nhận diện hàng tạp hóa
+      </h1>
+      <h2 className="text-white font-bold text-xl mt-4 mb-2">About us</h2>
+      <div className="text-white">
+        <div className="overview-table-row">
+          <div className="overview-table-cell">Nguyễn Trường Thịnh</div>
+          <div className="overview-table-cell">18521447</div>
+        </div>
+        <div className="overview-table-row">
+          <div className="overview-table-cell">Nguyễn Hoàng Thiên</div>
+          <div className="overview-table-cell">18521427</div>
+        </div>
+        <div className="overview-table-row">
+          <div className="overview-table-cell">Trần Hoàng Sơn</div>
+          <div className="overview-table-cell">18521351</div>
+        </div>
+        <div className="overview-table-row">
+          <div className="overview-table-cell">Đỗ Mạnh Quân</div>
+          <div className="overview-table-cell">18521283</div>
+        </div>
+      </div>
     </MainContainer>
   );
 }
